9f31c3e9c14755b31deb401683aa7b850089de31
thegambler1999 <vaisakhsatheesan2017@gmail.com>
2023-07-15 13:39:32 +0530

    Additional commit

--- dist/Component.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="types" />
-import UIComponent from "sap/ui/core/UIComponent";
-/**
- * @namespace ui5.typescript.helloworld
- */
-export default class Component extends UIComponent {
-    multiply(x: number, y: number): number;
-}
--- dist/Component.js
+++ dist/Component.js
@@ -1,9 +1,2 @@
-import UIComponent from "sap/ui/core/UIComponent";
-/**
- * @namespace ui5.typescript.helloworld
- */
-export default class Component extends UIComponent {
-    multiply(x, y) {
-        return x * y;
-    }
-}
+"use strict";sap.ui.define(["sap/ui/core/UIComponent","sap/ui/Device"],function(t,s){const n=s["support"];const i=t.extend("ui5typescriptfromscratch.Component",{metadata:{manifest:"json"},init:function s(){t.prototype.init.call(this)},getContentDensityClass:function t(){if(this.contentDensityClass===undefined){if(document.body.classList.contains("sapUiSizeCozy")||document.body.classList.contains("sapUiSizeCompact")){this.contentDensityClass=""}else if(!n.touch){this.contentDensityClass="sapUiSizeCompact"}else{this.contentDensityClass="sapUiSizeCozy"}}return this.contentDensityClass}});return i});
+//# sourceMappingURL=Component.js.map
